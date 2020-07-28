0a85205ce05de65f0910674ddffd11c790e5a937
KlivesDev <n.klivesvanirgames@gmail.com>
2020-07-28 17:15:48 +0100

    bruh

--- WatchinWeebsBot/ConfigJson.cs
+++ WatchinWeebsBot/ConfigJson.cs
@@ -7,6 +7,6 @@ namespace WatchinWeebsBot
 {
     public class ConfigJson
     {
-        public string Token = "NzM3MzI2NTY4MjcxMTE4NDU3.Xx7u4A.0rVEJsuCdQiy3sgZvgROG8P62b8";
+        public string Token = "";
     }
 }

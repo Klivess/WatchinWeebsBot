6de82b69d92972c94acb6e0817876639ad42c6c6
KlivesDev <n.klivesvanirgames@gmail.com>
2020-07-29 08:47:44 +0100

    try catch blocks for lazy crash fix

--- WatchinWeebsBot/Bot.cs
+++ WatchinWeebsBot/Bot.cs
@@ -82,21 +82,36 @@ namespace WatchinWeebsBot
 
         async Task Client_GuildMemberBanned(GuildBanAddEventArgs e)
         {
-            await e.Guild.GetChannel(691036573713563758).SendMessageAsync(e.Member.Mention + " has been banned.")
-                .Result.CreateReactionAsync(DiscordEmoji.FromUnicode("👋"));
+            try
+            {
+                await e.Guild.GetChannel(691036573713563758).SendMessageAsync(e.Member.Mention + " has been banned.")
+                    .Result.CreateReactionAsync(DiscordEmoji.FromUnicode("👋"));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         async Task Client_GuildMemberAdded(GuildMemberAddEventArgs e)
         {
-            await e.Guild.GetChannel(691036573713563758).SendMessageAsync("A new weeb has joined! Welcome " + e.Member.Mention + "!")
-                .Result.CreateReactionAsync(DiscordEmoji.FromUnicode("👋"));
+            try
+            {
+                await e.Guild.GetChannel(691036573713563758).SendMessageAsync("A new weeb has joined! Welcome " + e.Member.Mention + "!").Result.CreateReactionAsync(DiscordEmoji.FromUnicode("👋"));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public bool tracing = false;
 
         async Task Client_MessageSent(MessageCreateEventArgs e)
         {
-            /*
+            try
+            {
+                /*
             foreach(string b in bruh)
             {
                 if (e.Channel.Id.ToString() == b && e.Message.Author.Id.ToString() != "227462990293762049")
@@ -105,87 +120,112 @@ namespace WatchinWeebsBot
                 }
             }
             */
-            if (e.Message.Content.ToLower().Contains("hello nezuko"))
-            {
-                await e.Channel.SendMessageAsync("Hello!");
-            }
-            /*
-            if(e.Message.Content.ToLower().Contains("nezuko make role"))
-            {
-                string name = e.Message.Content.Replace("nezuko make role", string.Empty);
-                var role = e.Guild.CreateRoleAsync(name, null, DiscordColor.Blurple);
-                await e.Channel.SendMessageAsync("Role has been made!");
-            }
-            */
-            if (e.Message.Content.ToLower().Contains("make channel nezuko"))
-            {
-                if (e.Message.Author.Id.ToString() == "227462990293762049")
+                if (e.Message.Content.ToLower().Contains("hello nezuko"))
                 {
-                    string name = e.Message.Content.Replace("make channel nezuko", string.Empty);
-                    await e.Guild.CreateChannelAsync(name, 0);
-                    await e.Channel.SendMessageAsync("Channel has been made!");
+                    await e.Channel.SendMessageAsync("Hello!");
                 }
-            }
-            if (e.Message.Content.ToLower().Contains("delete channel nezuko"))
-            {
-                if (e.Message.Author.Id.ToString() == "227462990293762049")
+                /*
+                if(e.Message.Content.ToLower().Contains("nezuko make role"))
                 {
-                    await e.Message.MentionedChannels.ElementAtOrDefault(0).DeleteAsync();
-                    await e.Channel.SendMessageAsync("Channel has been deleted!");
+                    string name = e.Message.Content.Replace("nezuko make role", string.Empty);
+                    var role = e.Guild.CreateRoleAsync(name, null, DiscordColor.Blurple);
+                    await e.Channel.SendMessageAsync("Role has been made!");
                 }
-            }
-            if (e.Message.Content.ToLower().Contains("lock this chat"))
-            {
-                if (e.Message.Author.Id.ToString() == "227462990293762049")
+                */
+                if (e.Message.Content.ToLower().Contains("make channel nezuko"))
                 {
-                    bruh.Add(e.Message.Channel.Id.ToString());
-                    await e.Channel.SendMessageAsync("Channel has been locked!");
-                }
-            }
-            if (e.Message.Content.ToLower().Contains("unlock this chat"))
-            {
-                if (e.Message.Author.Id.ToString() == "227462990293762049")
-                {
-                    foreach(string item in bruh)
+                    if (e.Message.Author.Id.ToString() == "227462990293762049")
                     {
-                        if (item == e.Channel.Id.ToString())
+                        string name = e.Message.Content.Replace("make channel nezuko", string.Empty);
+                        await e.Guild.CreateChannelAsync(name, 0);
+                        await e.Channel.SendMessageAsync("Channel has been made!");
+                    }
+                }
+                if (e.Message.Content.ToLower().Contains("delete channel nezuko"))
+                {
+                    if (e.Message.Author.Id.ToString() == "227462990293762049")
+                    {
+                        await e.Message.MentionedChannels.ElementAtOrDefault(0).DeleteAsync();
+                        await e.Channel.SendMessageAsync("Channel has been deleted!");
+                    }
+                }
+                if (e.Message.Content.ToLower().Contains("lock this chat"))
+                {
+                    if (e.Message.Author.Id.ToString() == "227462990293762049")
+                    {
+                        bruh.Add(e.Message.Channel.Id.ToString());
+                        await e.Channel.SendMessageAsync("Channel has been locked!");
+                    }
+                }
+                if (e.Message.Content.ToLower().Contains("unlock this chat"))
+                {
+                    if (e.Message.Author.Id.ToString() == "227462990293762049")
+                    {
+                        foreach (string item in bruh)
                         {
+                            if (item == e.Channel.Id.ToString())
+                            {
 
+                            }
                         }
                     }
                 }
+                if (e.Message.Content.ToLower().Contains("!trace"))
+                {
+                    tracing = true;
+                    await e.Message.DeleteAsync();
+                }
+                if (e.Message.Content.ToLower().Contains("!untrace"))
+                {
+                    tracing = true;
+                    await e.Message.DeleteAsync();
+                }
+                if (tracing && e.Message.Author.Id.ToString() == "227462990293762049" && !e.Message.Content.Contains("!trace") && !e.Message.Content.Contains("!untrace"))
+                {
+                    await e.Message.Channel.SendMessageAsync(e.Message.Content);
+                    await e.Message.DeleteAsync();
+                }
             }
-            if (e.Message.Content.ToLower().Contains("!trace"))
+            catch(Exception ex)
             {
-                tracing = true;
-                await e.Message.DeleteAsync();
-            }
-            if (e.Message.Content.ToLower().Contains("!untrace"))
-            {
-                tracing = true;
-                await e.Message.DeleteAsync();
-            }
-            if (tracing && e.Message.Author.Id.ToString() == "227462990293762049" && !e.Message.Content.Contains("!trace") && !e.Message.Content.Contains("!untrace"))
-            {
-                await e.Message.Channel.SendMessageAsync(e.Message.Content);
-                await e.Message.DeleteAsync();
+                Console.WriteLine(ex.Message);
             }
         }
 
         async Task Client_MessageDeleted(MessageDeleteEventArgs e)
         {
+            try
+            {
 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         async Task Client_BanRemoved(GuildBanRemoveEventArgs e)
         {
-
+            try
+            {
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         async Task Client_MemberLeave(GuildMemberRemoveEventArgs e)
         {
-            await e.Guild.GetChannel(691036573713563758).SendMessageAsync("A person has left. Goodbye" + e.Member.Mention + "!")
-                .Result.CreateReactionAsync(DiscordEmoji.FromUnicode("👋"));
+            try
+            {
+                await e.Guild.GetChannel(691036573713563758).SendMessageAsync("A person has left. Goodbye" + e.Member.Mention + "!").Result.CreateReactionAsync(DiscordEmoji.FromUnicode("👋"));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
